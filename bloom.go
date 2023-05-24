b795c123bec798c34ff34b90d4822b7eb93d8d22
schou <pschou@users.noreply.github.com>
2023-05-24 15:22:53 -0400

    first commit

---
+++ bloom.go
@@ -0,0 +1,93 @@
+// Copyright 2020 github.com/pschou/go-bloom
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A fast bloom filter.
+
+package bloom
+
+import (
+	"encoding/gob"
+	"os"
+	"reflect"
+	"unsafe"
+
+	zxxh3 "github.com/zeebo/xxh3"
+)
+
+type Filter struct {
+	size uint64
+	dat  []byte
+}
+
+// Size will create a new bloom filter in memory, note that if the size is not a multiple of 8 it will be rounded down to the multiple of 8.
+func New(size int) *Filter {
+	return &Filter{size: uint64(size >> 3), dat: make([]byte, size>>3)}
+}
+
+// Test if the string may be in the filter
+func (f *Filter) TestString(s string) bool {
+	h := zxxh3.Hash(s2b(s))
+	return f.dat[(h>>3)%f.size]&(1<<(h&0x7)) > 0
+}
+
+// Test if a string may be in the filter
+func (f *Filter) Test(d []byte) bool {
+	h := zxxh3.Hash(d)
+	//fmt.Println("hash", h%f.size)
+	return f.dat[(h>>3)%f.size]&(1<<(h&0x7)) > 0
+}
+
+// Add a string to the filter
+func (f *Filter) AddString(s string) {
+	h := zxxh3.Hash(s2b(s))
+	f.dat[(h>>3)%f.size] |= 1 << (h & 0x7)
+}
+
+// Add a byte slice to the filter
+func (f *Filter) Add(d []byte) {
+	h := zxxh3.Hash(d)
+	//fmt.Println("hash", h%f.size)
+	f.dat[(h>>3)%f.size] |= 1 << (h & 0x7)
+}
+
+func s2b(value string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&value))
+	bh.Data = sh.Data
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	return b
+}
+
+type save struct {
+	Size uint64
+	Dat  []byte
+}
+
+// Save the filter into a file
+func (w *Filter) Save(fh *os.File) {
+	enc := gob.NewEncoder(fh)
+	enc.Encode(save{Size: w.size, Dat: w.dat})
+}
+
+// Load a filter from a file
+func Load(fh *os.File) (*Filter, error) {
+	dec := gob.NewDecoder(fh)
+	ret := new(save)
+	err := dec.Decode(ret)
+	if err != nil {
+		return nil, err
+	}
+	return &Filter{dat: ret.Dat, size: ret.Size}, nil
+}
---
+++ bloom_test.go
@@ -0,0 +1,94 @@
+package bloom
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func ExampleNew() {
+	filter := New(100)
+	fmt.Println("before", filter.dat)
+	filter.AddString("hello")
+	fmt.Println("after", filter.dat)
+	fmt.Println("test", filter.TestString("hello"))
+	// Output:
+	// before [0 0 0 0 0 0 0 0 0 0 0 0]
+	// after [0 0 0 32 0 0 0 0 0 0 0 0]
+	// test true
+}
+
+func ExampleAddString() {
+	filter := New(100)
+	fmt.Println("before", filter.dat)
+	filter.AddString("hello")
+	fmt.Println("after", filter.dat)
+	fmt.Println("test", filter.TestString("hello"))
+	// Output:
+	// before [0 0 0 0 0 0 0 0 0 0 0 0]
+	// after [0 0 0 32 0 0 0 0 0 0 0 0]
+	// test true
+}
+
+func ExampleAdd() {
+	filter := New(100)
+	fmt.Println("before", filter.dat)
+	filter.Add([]byte("hello"))
+	fmt.Println("after", filter.dat)
+	fmt.Println("test", filter.TestString("hello"))
+	// Output:
+	// before [0 0 0 0 0 0 0 0 0 0 0 0]
+	// after [0 0 0 32 0 0 0 0 0 0 0 0]
+	// test true
+}
+
+func ExampleSaveAndLoad() {
+	filter := New(100)
+	filter.Add([]byte("hello"))
+	fmt.Println("before", filter.dat)
+	fh, _ := os.Create("bloom.flt")
+	filter.Save(fh)
+	fh.Close()
+
+	my, _ := os.Open("bloom.flt")
+	filt, _ := Load(my)
+	my.Close()
+	fmt.Println("after", filt.dat)
+	fmt.Println("test", filt.TestString("hello"))
+	// Output:
+	// before [0 0 0 32 0 0 0 0 0 0 0 0]
+	// after [0 0 0 32 0 0 0 0 0 0 0 0]
+	// test true
+}
+
+func BenchmarkAdd(b *testing.B) {
+	dat := []byte("helloworld")
+	filter := New(1 << 32)
+	for n := 0; n < b.N; n++ {
+		filter.Add(dat)
+	}
+}
+
+func BenchmarkAddString(b *testing.B) {
+	dat := "helloworld"
+	filter := New(1 << 32)
+	for n := 0; n < b.N; n++ {
+		filter.AddString(dat)
+	}
+}
+
+func BenchmarkTest(b *testing.B) {
+	dat := []byte("helloworld")
+	filter := New(1 << 32)
+	for n := 0; n < b.N; n++ {
+		filter.Test(dat)
+	}
+}
+
+func BenchmarkTestString(b *testing.B) {
+	dat := "helloworld"
+	filter := New(1 << 32)
+	for n := 0; n < b.N; n++ {
+		filter.TestString(dat)
+	}
+}
